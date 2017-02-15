23a97b8de019c3f880e118cada3d8929ff4d5ec1
grismego <gris.mego@gmail.com>
2017-02-15 20:57:19 +0700

    1
    Доработки

--- js/js.js
+++ js/js.js
@@ -7,23 +7,22 @@ var close_map = document.querySelector(".close-form-map");
 
 
 
-    map.addEventListener("click", function(event) {
-        event.preventDefault();
-            bigmap.classList.add("modal-map-show");
-    });
+map.addEventListener("click", function(event) {
+  event.preventDefault();
+  bigmap.classList.add("modal-map-show");
+});
 
-    close_map.addEventListener("click", function(event) {
-        event.preventDefault();
-        bigmap.classList.remove("modal-map-show");
-    });
+close_map.addEventListener("click", function(event) {
+  event.preventDefault();
+  bigmap.classList.remove("modal-map-show");
+});
 
-      link.addEventListener("click", function(event) {
-          event.preventDefault();
-            popup.classList.add("modal-show");
-      });
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("modal-show");
+});
 
-      close.addEventListener("click", function(event)
-      {
-          event.preventDefault();
-          popup.classList.remove("modal-show");
-      });
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("modal-show");
+});
